8c3626ed31625c99158289ef91f9afa48a70c431
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-02-28 20:02:27 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(input)
+  i=3
+  while i<=input  #tried an ennumerable and a loop at first. They worked but were too slow for the largest number. Switched to a loop that only goes through odd numbers to find the first prime factor, and then divides the input until it gets the largest prime factor.
+    if i.prime? && input%i==0
+      high=i
+      input=input/i
+    else
+      i+=2 #to loop through ODD numbers, and ONLY when necessary
+    end #end of the if statement that checks if it's a factor
+  end #end of the factor-finding while loop
+ return high
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+require 'prime'
+class LargestPrimeFactor
+  def initialize(number)
+    @input=number
+    @number=1
+  end
+ def largest_prime_factor
+   i=3
+    while i<=@input #this is the iterator that finds factors of the input
+    if i.prime? && @input%i==0 #this checks that it's both prime and a factor
+      @number=i
+      @input=@input/i
+      else
+      i+=2
+    end #end of the if statement that checks if it's a factor
+  end #end of the loop that goes through all the odd numbers, checks if they're prime and a factor, and divides the input number so that it's easier to calculate the highest prime factor
+  return @number
+end
+  def number
+    @number=largest_prime_factor
+  end
+
+end
